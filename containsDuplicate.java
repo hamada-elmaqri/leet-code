244ed64c0e190a2cac47bbef813ba56b1403b920
hamada-elmaqri <elmaqrih01@gmail.com>
2024-12-02 11:31:48 +0100

    217. Contains Duplicate java

---
+++ containsDuplicate.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        /*Time Limit Exceeded */
+    /*    
+    //Input: nums = [1,2,3,1]
+    int n = nums.length;
+        for (int i =0 ; i<n-1; i++){
+            for (int j = i+1 ; j<n; j++){
+                 if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }*/
+        HashSet<Integer> numbers = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numbers.contains(nums[i])) {
+                return true;
+            }
+            numbers.add(nums[i]);
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1,2,3,1};//Input: nums = [1,2,3,1] Output: true
+        int[] nums2 = {1,2,3,4};//Input: nums = [1,2,3,4] Output: false
+        int[] nums3 = {1,1,1,3,3,4,3,2,4,2};//Input: nums = [1,1,1,3,3,4,3,2,4,2] Output: true
+        System.out.println(solution.containsDuplicate(nums1));
+        System.out.println(solution.containsDuplicate(nums2));
+        System.out.println(solution.containsDuplicate(nums3));
+    }
+}
+
